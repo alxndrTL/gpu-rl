f3a73ac7f6170cf062edca27c317013eed73e430
Alexandre TL <alextorresleguet@icloud.com>
2025-02-18 22:04:22 +0100

    custom modifs

--- grpo_demo.py
+++ grpo_demo.py
@@ -1,12 +1,11 @@
-# train_grpo.py
+# train_grpo.py (https://gist.github.com/willccbb/4676755236bb08cab5f4e54a0475d6fb)
 #
-# See https://github.com/willccbb/verifiers for ongoing developments
+# pip install git+https://github.com/huggingface/trl.git accelerate transformers datasets peft wandb tqdm
 #
 import re
 import torch
 from datasets import load_dataset, Dataset
 from transformers import AutoTokenizer, AutoModelForCausalLM
-from peft import LoraConfig
 from trl import GRPOConfig, GRPOTrainer
 
 # Load and prep dataset
@@ -104,13 +103,14 @@ def xmlcount_reward_func(completions, **kwargs) -> list[float]:
     contents = [completion[0]["content"] for completion in completions]
     return [count_xml(c) for c in contents]
 
-#model_name = "meta-llama/Llama-3.2-1B-Instruct"
-model_name = "Qwen/Qwen2.5-1.5B-Instruct"
+model = "Qwen" # Llama, Qwen
 
-if "Llama" in model_name:
+if model == "Llama":
+    model_name = "meta-llama/Llama-3.2-1B-Instruct"
     output_dir = "outputs/Llama-1B-GRPO"
     run_name = "Llama-1B-GRPO-gsm8k"
 else:
+    model_name = "Qwen/Qwen2.5-1.5B-Instruct"
     output_dir="outputs/Qwen-1.5B-GRPO"
     run_name="Qwen-1.5B-GRPO-gsm8k"
     
@@ -136,13 +136,6 @@ training_args = GRPOConfig(
     report_to="wandb",
     log_on_each_node=False,
 )
-peft_config = LoraConfig(
-    r=16,
-    lora_alpha=64,
-    target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "up_proj", "down_proj", "gate_proj"],
-    task_type="CAUSAL_LM",
-    lora_dropout=0.05,
-)
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
     torch_dtype=torch.bfloat16,
@@ -153,7 +146,6 @@ model = AutoModelForCausalLM.from_pretrained(
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 tokenizer.pad_token = tokenizer.eos_token
 
-# use peft at your own risk; not working for me with multi-GPU training
 trainer = GRPOTrainer(
     model=model,
     processing_class=tokenizer,
@@ -165,6 +157,5 @@ trainer = GRPOTrainer(
         correctness_reward_func],
     args=training_args,
     train_dataset=dataset,
-    #peft_config=peft_config
 )
 trainer.train()
