e75ef44bd00f53b5f725a07dd80365f8bcebd2a2
Alexandre TL <alextorresleguet@icloud.com>
2025-02-18 22:08:02 +0100

    cleaning

--- grpo_demo.py
+++ grpo_demo.py
@@ -32,15 +32,15 @@ def extract_hash_answer(text: str) -> str | None:
 
 # uncomment middle messages for 1-shot prompting
 def get_gsm8k_questions(split = "train") -> Dataset:
-    data = load_dataset('openai/gsm8k', 'main')[split] # type: ignore
-    data = data.map(lambda x: { # type: ignore
+    data = load_dataset('openai/gsm8k', 'main')[split]
+    data = data.map(lambda x: {
         'prompt': [
             {'role': 'system', 'content': SYSTEM_PROMPT},
             {'role': 'user', 'content': x['question']}
         ],
         'answer': extract_hash_answer(x['answer'])
-    }) # type: ignore
-    return data # type: ignore
+    })
+    return data
 
 dataset = get_gsm8k_questions()
 
