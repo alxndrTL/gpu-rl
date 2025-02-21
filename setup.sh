dc87dd66e49ccd560d719c259578df1884782517
Alexandre TL <alextorresleguet@icloud.com>
2025-02-21 11:24:02 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,9 +12,11 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 # Install custom TRL branch for memory optimization
-echo "=== Installing TRL ==="
-git clone https://github.com/huggingface/trl.git trl_custom
+echo "=== Installing custom TRL branch ==="
+git clone --branch grpo-vram-optimization https://github.com/andyl98/trl.git trl_custom
 cd trl_custom
+# Tested, stable commit
+git checkout ccc95472f6245f2db00986a08ca16da68bf32c14
 pip install .
 cd ..
 
