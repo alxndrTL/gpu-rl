31f1ee812fdbb8e8fc42812fd71a865df33f869a
alxndrTL <alextorresleguet@icloud.com>
2025-02-22 11:15:44 +0000

    organizing files

--- basic_trl.sh
+++
@@ -1,6 +0,0 @@
-# Install custom TRL branch for memory optimization
-echo "=== Installing custom TRL branch ==="
-git clone https://github.com/huggingface/trl.git trl
-cd trl
-pip install --force-reinstall .
-cd ..
--- setup.sh
+++ setup.sh
@@ -11,13 +11,10 @@ echo "=== Installing requirements ==="
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Install custom TRL branch for memory optimization
-echo "=== Installing custom TRL branch ==="
-git clone --branch grpo-vram-optimization https://github.com/andyl98/trl.git trl_custom
-#git clone https://github.com/huggingface/trl.git trl_custom
-cd trl_custom
-# Tested, stable commit
-git checkout ccc95472f6245f2db00986a08ca16da68bf32c14
+# Install TRL
+echo "=== Installing TRL ==="
+git clone https://github.com/huggingface/trl.git trl
+cd trl
 pip install .
 cd ..
 
